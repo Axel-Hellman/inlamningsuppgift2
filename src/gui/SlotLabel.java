6f5ffa53a5c590fd7835e7c5e6f044ed407c4553
AxelHellman <axel.hellman@outlook.com>
2018-10-12 14:00:20 +0200

    Implement SlotLable
    Add clear in StatusLable

--- src/gui/SlotLabel.java
+++ src/gui/SlotLabel.java
@@ -6,44 +6,65 @@ import java.awt.event.MouseListener;
 import java.util.Observable;
 import java.util.Observer;
 
+import model.Sheet;
+
 public class SlotLabel extends ColoredLabel implements Observer, MouseListener {
-	public SlotLabel() {
+
+	private String name;
+	private CurrentCell currentCell;
+	private Sheet sheet;
+	private StatusLabel statusLabel;
+
+	public SlotLabel(String name, CurrentCell currentCell, Sheet sheet, StatusLabel statusLabel) {
 		super("                    ", Color.WHITE, RIGHT);
+		this.name = name;
+		this.currentCell = currentCell;
+		this.sheet = sheet;
+		addMouseListener(this);
+		sheet.addObserver(this);
 	}
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
+	public String getName() {
+		return name;
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
+		String s = sheet.getCellValue(name);
+		setText(s);
+	}
 
+	public void updateText() {
+		String s = sheet.getCellValue(name);
+		setText(s);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		currentCell.setWhite();
+		setBackground(Color.YELLOW);
+		currentCell.set(this);
+		
+		if(statusLabel != null) {
+			statusLabel.clear();
+		}
+		
+		System.out.println(currentCell.getName());
+	}
+
+	// Not used
+	public void mousePressed(MouseEvent e) {
+	}
+
+	// Not used
+	public void mouseReleased(MouseEvent e) {
+	}
+
+	// Not used
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	// Not used
+	public void mouseExited(MouseEvent e) {
 	}
 }
