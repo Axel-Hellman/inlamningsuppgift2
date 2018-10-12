7dc63187d9b85ecf6e354e740e77e89ebf0e88f2
JoakimRudberg <rudberg.joakim@gmail.com>
2018-10-12 13:37:48 +0200

    Add class CurrentCell

---
+++ src/gui/CurrentCell.java
@@ -0,0 +1,30 @@
+package gui;
+
+import java.awt.Color;
+import java.util.Observable;
+
+public class CurrentCell extends Observable{
+	private SlotLabel currentCell;
+	
+	public CurrentCell(){
+		
+	}
+	
+	public String getName(){
+		return currentCell.getName();
+	}
+	
+	public void set(SlotLabel slotLabel){
+		currentCell = slotLabel;
+		setChanged();
+		notifyObservers();
+	}
+	
+	public void setWhite(){
+		currentCell.setBackground(Color.WHITE);
+	}
+	
+	public void clearSlot(){
+		currentCell.setText("");
+	}
+}
