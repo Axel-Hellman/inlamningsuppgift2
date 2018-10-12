a2fe18c5384e926448d9e7a487dab07882c5378c
JoakimRudberg <rudberg.joakim@gmail.com>
2018-10-12 11:30:12 +0200

    Implement CurrentCell

--- src/model/CurrentCell.java
+++ src/model/CurrentCell.java
@@ -1,5 +1,21 @@
 package model;
 
 public class CurrentCell {
+	
+	private Cell cell;
+	private String address;
 
+	public void setCurrentCell(String address){
+		this.address = address;
+		// cell = Sheet.getCell(address);
+	}
+	
+	public Cell getCell(){
+		return cell;
+	}
+	
+	public String getAddress(){
+		return address;
+	}
+	
 }
