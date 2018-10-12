9676e3b82cf612058d8c12d4d3b33a56636cf448
JoakimRudberg <rudberg.joakim@gmail.com>
2018-10-12 15:36:02 +0200

    Implement LoadMenyItem, XLBufferedReader

--- src/gui/menu/LoadMenuItem.java
+++ src/gui/menu/LoadMenuItem.java
@@ -2,17 +2,36 @@ package gui.menu;
 
 import gui.StatusLabel;
 import gui.XL;
+import model.Cell;
+import model.Sheet;
+import util.XLBufferedReader;
+
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+
 import javax.swing.JFileChooser;
 
 class LoadMenuItem extends OpenMenuItem {
+	private Sheet sheet;
  
-    public LoadMenuItem(XL xl, StatusLabel statusLabel) {
+    public LoadMenuItem(XL xl, StatusLabel statusLabel, Sheet sheet) {
         super(xl, statusLabel, "Load");
+        this.sheet = sheet;
     }
 
     protected void action(String path) throws FileNotFoundException {
-        // TODO
+        statusLabel.clear();
+        try {
+        	XLBufferedReader XLbr = new XLBufferedReader(path);
+        	HashMap<String, Cell> temp = new HashMap<String, Cell>();
+        	XLbr.load(temp);
+        	System.out.println(temp.size());
+        	sheet.clearAll();
+        	sheet.load(temp);
+        	sheet.updateSlotLabels();
+        } catch(Exception e) {
+        	statusLabel.setText(e.getMessage());
+        }
     }
 
     protected int openDialog(JFileChooser fileChooser) {
--- src/util/XLBufferedReader.java
+++ src/util/XLBufferedReader.java
@@ -4,20 +4,27 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Map;
+import model.Factory;
+import model.Cell;
 
 //TODO move to another package
 public class XLBufferedReader extends BufferedReader {
+	
+	private Factory factory = new Factory();
+	
     public XLBufferedReader(String name) throws FileNotFoundException {
         super(new FileReader(name));
     }
 
     // TODO Change Object to something appropriate
-    public void load(Map<String, Object> map) {
+    public void load(Map<String, Cell> map) {
         try {
             while (ready()) {
                 String string = readLine();
                 int i = string.indexOf('=');
-                // TODO
+                String name = string.substring(0, i);
+                String expr = string.substring(i+1);
+                map.put(name, factory.cell(expr));
             }
         } catch (Exception e) {
             throw new XLException(e.getMessage());
