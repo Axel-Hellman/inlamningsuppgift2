c1c890ce218a78d2e0899463de623526f9b073af
Treedill <johannes@tornblom.eu>
2018-10-12 15:11:31 +0200

    Some fixes in SaveMenu

--- src/gui/menu/SaveMenuItem.java
+++ src/gui/menu/SaveMenuItem.java
@@ -2,16 +2,24 @@ package gui.menu;
 
 import gui.StatusLabel;
 import gui.XL;
+import model.Sheet;
+import util.XLPrintStream;
+
 import java.io.FileNotFoundException;
 import javax.swing.JFileChooser;
 
 class SaveMenuItem extends OpenMenuItem {
-    public SaveMenuItem(XL xl, StatusLabel statusLabel) {
+	
+	private Sheet sheet;
+	
+    public SaveMenuItem(XL xl, StatusLabel statusLabel, Sheet sheet) {
         super(xl, statusLabel, "Save");
+        this.sheet = sheet;
     }
 
     protected void action(String path) throws FileNotFoundException {
-        // TODO
+        XLPrintStream writer = new XLPrintStream(path);
+        writer.save(sheet.entrySet());
     }
 
     protected int openDialog(JFileChooser fileChooser) {
