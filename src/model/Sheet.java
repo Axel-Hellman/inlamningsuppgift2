474125be7a78d7ee3cfd987a1ddedf9216b1d8d5
AxelHellman <axel.hellman@outlook.com>
2018-10-12 10:46:05 +0200

    Add skeleton to Sheet

--- src/model/Sheet.java
+++ src/model/Sheet.java
@@ -1,41 +1,28 @@
 package model;
 
 import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Observable;
+import java.util.Set;
 
 import expr.Environment;
 import gui.SlotLabels;
 
-public class Sheet extends Observable implements Environment, Cell {
+public class Sheet extends Observable implements Environment {
 
-	private HashMap<String,Cell> sheet;
+	private HashMap<String, Cell> sheet;
 	private Factory factory;
 	private SlotLabels slotLabels;
-	
-	public Sheet(){
-		super(); 
-		sheet = new HashMap<String,Cell>();
+
+	public Sheet() {
+		super();
+		sheet = new HashMap<String, Cell>();
 		factory = new Factory();
 	}
-	
-	public Sheet(HashMap<String,Cell> sheet){
+
+	public Sheet(HashMap<String, Cell> sheet) {
 		this.sheet = sheet;
-		
-	}
-	
-	public String getComment(){
-		return null;
-	}
-	
-	public String toString(Environment e){
-		return null;
-	}
 
-
-	@Override
-	public double value(Environment env) {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 
 	@Override
@@ -44,10 +31,60 @@ public class Sheet extends Observable implements Environment, Cell {
 		return 0;
 	}
 
+	public Cell getCell(String key) {
+		return null;
+	}
+
+	public String getCellValue(String key) {
+		return null;
+	}
+
+	public String getStringValue() {
+		return null;
+	}
+
+	public void put(String string, Cell cell) {
+
+	}
+
+	// Vet inte vad detta är, förklara.
+	// public Set<Entry<String, Slot>> EntrySet(){
+	//
+	// }
+
+	public int size() {
+		return 0;
+	}
+
+	public void removeCell(String key) {
+
+	}
+
+	public void clearAll() {
+
+	}
+
+	public void updateSheet() {
+
+	}
+
+	public void putCell(String name, String text) {
+
+	}
+
+	public void load(HashMap<String, Cell> newSheet) {
+
+	}
+
+	private void checkLoop(String name, Cell cell) {
+
+	}
+
+	public void addSlotLabels(SlotLabels slotLabels) {
+
+	}
+
+	public void updateSlotLabels() {
+
+	}
 }
-
-
-	
-	
-	
-	
