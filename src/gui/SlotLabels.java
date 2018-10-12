78376b4b95e7262e65d9dc9f5e9de10228bce76b
AxelHellman <axel.hellman@outlook.com>
2018-10-12 14:34:26 +0200

    Implement SlotLables

--- src/gui/SlotLabels.java
+++ src/gui/SlotLabels.java
@@ -5,27 +5,48 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.SwingConstants;
 
-public class SlotLabels extends GridPanel {
-    private List<SlotLabel> labelList;
+import com.sun.xml.internal.bind.v2.runtime.Name;
 
-    public SlotLabels(int rows, int cols) {
-        super(rows + 1, cols);
-        labelList = new ArrayList<SlotLabel>(rows * cols);
-        for (char ch = 'A'; ch < 'A' + cols; ch++) {
-            add(new ColoredLabel(Character.toString(ch), Color.LIGHT_GRAY,
-                    SwingConstants.CENTER));
-        }
-        for (int row = 1; row <= rows; row++) {
-            for (char ch = 'A'; ch < 'A' + cols; ch++) {
-                SlotLabel label = new SlotLabel();
-                add(label);
-                labelList.add(label);
-            }
-        }
-        SlotLabel firstLabel = labelList.get(0); //  <- SÄTTER FÖRSTA LABELN
-        firstLabel.setBackground(Color.YELLOW);
-        
-    }
-    
-    
+import model.Sheet;
+
+public class SlotLabels extends GridPanel {
+	private List<SlotLabel> labelList;
+	private CurrentCell currentCell;
+	private Sheet sheet;
+	private StatusLabel statusLabel;
+
+	public SlotLabels(int rows, int cols, CurrentCell currentSlot, Sheet sheet, StatusLabel statusLabel) {
+		super(rows + 1, cols);
+		this.currentCell = currentSlot;
+		this.sheet = sheet;
+		this.statusLabel = statusLabel;
+
+		labelList = new ArrayList<SlotLabel>(rows * cols);
+		for (char ch = 'A'; ch < 'A' + cols; ch++) {
+			add(new ColoredLabel(Character.toString(ch), Color.LIGHT_GRAY, SwingConstants.CENTER));
+		}
+
+		for (int row = 1; row <= rows; row++) {
+			for (char ch = 'A'; ch < 'A' + cols; ch++) {
+				String name = ch + String.valueOf(row);
+				SlotLabel label = new SlotLabel(name, currentSlot, sheet, statusLabel);
+				add(label);
+				labelList.add(label);
+			}
+		}
+		SlotLabel firstLabel = labelList.get(0); // <- SÄTTER FÖRSTA LABELN
+		firstLabel.setBackground(Color.YELLOW);
+		currentCell.set(firstLabel);
+	}
+
+	public void clearAll() {
+		for (SlotLabel sl : labelList) {
+			sl.setText("");
+		}
+	}
+
+	public List<SlotLabel> getList() {
+		return labelList;
+	}
+
 }
