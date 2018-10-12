73eeaf1707a24f20d41c5be4de49d5f7e7fc6c38
AxelHellman <axel.hellman@outlook.com>
2018-10-12 13:28:00 +0200

    Add skeleton to SlotLable

--- src/gui/SlotLabel.java
+++ src/gui/SlotLabel.java
@@ -1,9 +1,49 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Observable;
+import java.util.Observer;
 
-public class SlotLabel extends ColoredLabel {
-    public SlotLabel() {
-        super("                    ", Color.WHITE, RIGHT);
-    }
+public class SlotLabel extends ColoredLabel implements Observer, MouseListener {
+	public SlotLabel() {
+		super("                    ", Color.WHITE, RIGHT);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+
+	}
 }
