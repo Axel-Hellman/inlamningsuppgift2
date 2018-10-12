a0c7758565a356b6db4a1323e223f4f1da3ba2a3
Treedill <johannes@tornblom.eu>
2018-10-12 14:20:57 +0200

    SheetPanel implemented

--- src/gui/SheetPanel.java
+++ src/gui/SheetPanel.java
@@ -3,9 +3,13 @@ package gui;
 import static java.awt.BorderLayout.CENTER;
 import static java.awt.BorderLayout.WEST;
 
+import model.Sheet;
+
 public class SheetPanel extends BorderPanel {
-    public SheetPanel(int rows, int columns) {
+    public SheetPanel(int rows, int columns, CurrentCell currentCell, Sheet sheet, StatusLabel statusLabel) {
         add(WEST, new RowLabels(rows));
+        slotLabels = new SlotLabels(rows, columns, currentCell, sheet, statusLabel);
         add(CENTER, new SlotLabels(rows, columns));
+        sheet.addSlotLabels(slotLabels);
     }
 }
